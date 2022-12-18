2dbf7c6d6dee6a943545923dacd89d90e51432c0
Cappy Ishihara <cappy@cappuchino.xyz>
2022-12-18 22:01:28 +0700

    update stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,24 @@ import {
   Avatar,
   Badge,
   Card,
+  Collapse,
   Grid,
   Link,
   Spacer,
   Text,
 } from "@nextui-org/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import {
+  faGithub,
+  faGithubAlt,
+  faGitlab,
+  faMastodon,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,11 +34,15 @@ export default function Home() {
         <title>Pornpipat Popum</title>
         <meta name="description" content="Personal website and portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="twitter" content="https://twitter.com/CappyIshihara" />
+        <meta name="author" content="Pornpipat Popum" />
+        <meta
+          name="keywords"
+          content="pornpipat, popum, cappy, cappyishihara, korewachino, React, nextjs"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">
-        {/* move card to vertical center, slightly to the left */}
-
+      <main>
         <Grid.Container className="flex justify-center items-center h-screen absolute right-1/4">
           <Card css={{ mw: "500px" }} variant="bordered" className="card">
             <Card.Header>
@@ -39,26 +56,65 @@ export default function Home() {
                   Pornpipat Popum
                 </Text>
               </Grid.Container>
+              <Link href="https://en.pronouns.page/@korewachino">
+                <Badge color="warning" className="text-yellow-900">
+                  <Text className="text-black text-xs">Any pronouns</Text>
+                </Badge>
+              </Link>
             </Card.Header>
             <Card.Body>
-              <Text className="txtbody">
-                Pronouns: <Badge color="warning">any</Badge>
-              </Text>
-              <Text className="txtbody">Location: Thailand</Text>
-              <Spacer y={0.5} />
-              <Text className="text-4xl dark:text-white">Skills</Text>
+              <Collapse.Group className="" bordered>
+                <Collapse title="General">
+                  <Text className="txtbody">Location: Thailand</Text>
+                  <Spacer />
+                  <Text className="txtbody">Gender: Genderfluid</Text>
+                </Collapse>
 
-              <Text className="txtbody">
-                Programming Languages: Rust, Python, TypeScript (JavaScript), Go
-                <br />
-                Frameworks: Next.js, React, Nuxt.js, Vue.js, Flask, Rocket,
-                Tokio
-              </Text>
-              <Text className="txtbody">
-                Technologies: Docker, Kubernetes, Ansible, Git, Linux
-                (Fedora/RHEL)
-              </Text>
+                <Collapse title="Skills">
+                  <Text className="text-2xl dark:text-white">
+                    Programming Languages
+                  </Text>
+                  <Text className="txtbody">
+                    Rust, Python, TypeScript (JavaScript), Go Frameworks:
+                    Next.js, React, Nuxt.js, Vue.js, Flask, Rocket, Tokio
+                  </Text>
+                  <Spacer />
+                  <Text className="text-2xl dark:text-white">Frameworks</Text>
+                  <Text className="txtbody">
+                    Next.js, React, Nuxt.js, Vue.js, Flask, Rocket, Tokio
+                  </Text>
+                  <Spacer />
+
+                  <Text className="text-2xl dark:text-white">Technologies</Text>
+                  <Text className="txtbody">
+                    Docker, Kubernetes, Ansible, Git, Linux (Fedora/RHEL)
+                  </Text>
+                </Collapse>
+              </Collapse.Group>
             </Card.Body>
+            <Card.Footer>
+              {/* socials */}
+              <Grid.Container
+                className="flex p-5 justify-center text-4xl space-x-2"
+                xl={true}
+              >
+                <Link block href="https://twitter.com">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </Link>
+                <Link block href="https://github.com/korewaChino">
+                  <FontAwesomeIcon icon={faGithub} />
+                </Link>
+                <Link block href="https://gitlab.com/korewaChino">
+                  <FontAwesomeIcon icon={faGitlab} />
+                </Link>
+                <Link block href="mailto:cappy@cappuchino.xyz">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </Link>
+                <Link rel="me" href="https://mstdn.social/@korewaChino">
+                  <FontAwesomeIcon icon={faMastodon} />
+                </Link>
+              </Grid.Container>
+            </Card.Footer>
           </Card>
         </Grid.Container>
 
